3b2ebdea4e968a3246bb9dd4ad39d8a5bb5c5dd8
James Hood <james@jlhood.com>
2019-01-18 12:12:11 -0800

    Implement app

--- src/myfunction.py
+++ src/myfunction.py
@@ -3,11 +3,39 @@
 # must be the first import in files with lambda function handlers
 import lambdainit  # noqa: F401
 
-import lambdalogging
+import decimal
+import json
+import random
 
-LOG = lambdalogging.getLogger(__name__)
+SYMBOLS = [
+    'AMZN',
+    'AAPL',
+    'GOOG',
+    'TSLA',
+    'FB',
+]
+SECTOR = [
+    'HEALTHCARE',
+    'TECH',
+    'FINANCE'
+]
 
 
 def handler(event, context):
     """Lambda function handler."""
-    LOG.info('Received event: %s', event)
+    for _ in range(10):
+        # using print so the log is only JSON data
+        print(json.dumps(_generate_json_data()))
+
+
+def _generate_json_data():
+    return {
+        'ticker_symbol': random.choice(SYMBOLS),
+        'sector': random.choice(SECTOR),
+        'change': float(_random_decimal_between(1.12, 3.13)),
+        'price': float(_random_decimal_between(23.45, 98.76))
+    }
+
+
+def _random_decimal_between(a, b):
+    return decimal.Decimal(random.randrange(int(a * 100), int(b * 100))) / 100
